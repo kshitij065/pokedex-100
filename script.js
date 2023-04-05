b5f38fa35c4477c13208f766ec2250727f3b98f3
kshitij065 <129906291+kshitij065@users.noreply.github.com>
2023-04-05 16:10:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,19 @@
-const form = document.getElementById('form');
+const refreshButton = document.getElementById('refresh-button');
+const searchInput = document.getElementById('search-input');
 const pokemonListElement = document.getElementById('pokemon-list');
+const paginationElement = document.getElementById('pagination');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const number = form.number.value;
-  getPokemonData(number);
-});
+let currentPage = 1;
+const pageSize = 20;
+let totalPageCount = 1;
 
-async function getPokemonData(number) {
-  pokemonListElement.innerHTML = '';
-  for (let i = 1; i <= number; i++) {
-    const id = Math.floor(Math.random() * 898) + 1;
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemonData = await response.json();
+async function getRandomPokemonData(page) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${pageSize}&offset=${(page - 1) * pageSize}`);
+  const data = await response.json();
 
-    const pokemon = {
-      name: pokemonData.name,
-      image: pokemonData.sprites.other['official-artwork'].front_default,
-      type: pokemonData.types.map(type => type.type.name),
-      description: ''
-    };
+  const pokemonDataPromises = data.results.map(async result => {
+    const pokemonResponse = await fetch(result.url);
+    return pokemonResponse.json();
+  });
 
-    const speciesResponse = await fetch(pokemonData.species.url);
-    const speciesData = await speciesResponse.json();
-
-    const descriptionObject = speciesData.flavor_text_entries.find(
-      entry => entry.language.name === 'en'
-    );
-    pokemon.description = descriptionObject.flavor_text;
-
-    renderPokemon(pokemon);
-  }
-}
-
-function renderPokemon(pokemon) {
-  const pokemonElement = document.createElement('li');
-  pokemonElement.innerHTML = `
-    <img src="${pokemon.image}" alt="${pokemon.name}">
-    <h2>${pokemon.name}</h2>
-    <p>Type: ${pokemon.type.join(', ')}</p>
-    <p>${pokemon.description}</p>
-  `;
-  pokemonListElement.appendChild(pokemonElement);
-}
+  const
