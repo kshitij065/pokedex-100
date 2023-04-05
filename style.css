22a3c43047af4edc6833e8df1ebab82a7b2cca89
kshitij065 <kshitijkumar0282@gmail.com>
2023-04-05 15:54:47 +0530

    forth commit

---
+++ style.css
@@ -0,0 +1,40 @@
+header {
+  background-color: #f44336;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+
+main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+li {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  min-width: 200px;
+}
+
+img {
+  width: 150px;
+  height: 150px;
+}
+
+h2 {
+  margin: 10px 0 5px;
+  font-size: 24px;
+}
+
+p {
+  margin: 5px 0;
+}
